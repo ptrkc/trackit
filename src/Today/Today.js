a9490b16ace6a87c1fc27306a84f5c3d937d9c50
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-05-20 03:45:11 -0300

    Started Today screen

--- src/Today/Today.js
+++ src/Today/Today.js
@@ -1,155 +1,76 @@
-import { useContext } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../contexts/UserContext";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
 
-export default function Today() {
+export default function Habits() {
+    const [todayHabits, setTodayHabits] = useState([]);
     const { user } = useContext(UserContext);
-    console.log(user);
+
+    const today = dayjs().locale("pt-br").format("dddd, DD/MM");
+    useEffect(() => {
+        getTodayHabits();
+    }, []);
+
+    function getTodayHabits() {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${user.token}`,
+            },
+        };
+        const todayHabitsRequest = axios.get(
+            "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today",
+            config
+        );
+        todayHabitsRequest.then((response) => {
+            console.log(response.data);
+            setTodayHabits([...response.data]);
+        });
+        todayHabitsRequest.catch((error) => console.log(error.response.data));
+    }
     return (
-        <>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-            <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
-                deserunt nulla maiores. Fugit delectus assumenda quos nobis! Et,
-                ullam facere corporis minima sapiente animi, eos iste voluptatem
-                similique vel eligendi.
-            </p>
-        </>
+        <StyledDiv>
+            <div className="date">{today}</div>
+            {/* {showNewHabitCard ? (
+                <NewHabitCard
+                    setShowNewHabitCard={setShowNewHabitCard}
+                    getHabits={getHabits}
+                />
+            ) : (
+                ""
+            )} */}
+            {todayHabits.map((habit) => {
+                return (
+                    <span>
+                        {habit.id} {habit.name} {habit.done}{" "}
+                        {habit.currentSequence} {habit.highestSequence}{" "}
+                    </span>
+                );
+            })}
+            {!todayHabits.length ? (
+                <p className="no-habits">
+                    Você não tem nenhum hábito cadastrado para hoje!
+                </p>
+            ) : (
+                ""
+            )}
+        </StyledDiv>
     );
 }
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 0px 18px;
+    font-size: 18px;
+    line-height: 22px;
+    .date {
+        font-size: 23px;
+        color: ${(props) => props.theme.darkAccentColor};
+    }
+    .no-habits {
+        margin-top: 8px;
+    }
+`;
